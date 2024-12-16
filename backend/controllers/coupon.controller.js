7ec04cd5866abc68b62f5e85859d149ac076ea3a
KhoaTranAnh12 <131005608+KhoaTranAnh12@users.noreply.github.com>
2024-12-16 09:15:58 +0700

    added coupon controller

---
+++ backend/controllers/coupon.controller.js
@@ -0,0 +1,79 @@
+import { now } from "mongoose";
+import Coupon from "../models/coupon.model.js";
+
+export const getCoupon = async (req, res) => {
+	try {
+		const coupon = await Coupon.findOne({ isActive: true });
+		res.json(coupon || null);
+	} catch (error) {
+		console.log("Error in getCoupon controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+
+export const validateCoupon = async (req, res) => {
+	try {
+		const { code } = req.body;
+		const coupon = await Coupon.findOne({ code: code, isActive: true });
+
+		if (!coupon) {
+			return res.status(404).json({ message: "Coupon not found" });
+		}
+
+		if (coupon.expirationDate < new Date()) {
+			coupon.isActive = false;
+			await coupon.save();
+			return res.status(404).json({ message: "Coupon expired" });
+		}
+
+		res.json({
+			message: "Coupon is valid",
+			code: coupon.code,
+			discountPercentage: coupon.discountPercentage,
+		});
+	} catch (error) {
+		console.log("Error in validateCoupon controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+// Phần thêm vào
+export const getAllCoupons = async (req, res) => {
+	try {
+		const coupons = await Coupon.find({}); // find all coupons
+		res.json({ coupons });
+	} catch (error) {
+		console.log("Error in getAllcoupons controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+
+export const deleteCoupons = async (req, res) => {
+    try{
+        const coupon = await Coupon.findById(req.params.id);
+        if (!coupon) {
+            return res.status(404).json({ message: "Coupon not found" });
+        }
+        await Coupon.findByIdAndDelete(req.params.id);
+        res.json({ message: "Coupon deleted successfully" });
+    }
+    catch (error){
+        console.log("Error in deleteCoupons controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+export const createCoupon = async (req, res) => {
+	try {
+		const { code, discountPercentage, activeDays } = req.body;
+		let expirationDate = new Date();
+		expirationDate = expirationDate.setDate(expirationDate.getDate()+activeDays);
+		const coupon = await Coupon.create({
+			code, 
+			discountPercentage, 
+			expirationDate
+		});
+		res.status(201).json(coupon);
+	} catch (error) {
+		console.log("Error in createCoupon controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
