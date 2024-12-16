8cf87d6bf96495f930c09e013a4704c79f9bface
KhoaTranAnh12 <131005608+KhoaTranAnh12@users.noreply.github.com>
2024-12-16 09:32:06 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js"
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
 import userRoutes from "./routes/users.route.js"
@@ -30,6 +31,7 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/order", orderRoutes);
+app.use("/api/coupon", couponRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
@@ -43,5 +45,3 @@ app.listen(PORT, () => {
 	console.log("Server is running on http://localhost:" + PORT);
 	connectDB();
 });
-
-//k245416587,0AmzROostSqiQRHd
